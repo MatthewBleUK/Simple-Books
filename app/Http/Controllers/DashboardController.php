044c51c0d5cabda8605bb6e5250cf755b83a0000
Matthew Bleathman <24397291+MatthewBleUK@users.noreply.github.com>
2021-09-07 15:27:53 +0100

    Preparing to render transactions into view

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+
+    public function __construct() 
+    {
+        $this->middleware(['auth']);
+    } 
+
+    public function index ()
+    {   
+        //dd(auth()->user()->transactions);   //Uses collection  -> helps manipulate lists of data
+       
+       $transactions = Transaction::get();
+       
+        return view('dashboard', [
+            'transactions' => $transactions
+        ]);
+    }
+}
---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+
+class TransactionController extends Controller
+{
+    public function store(Request $request) 
+    {
+        // Validate
+
+        // Store
+        $request->user()->transactions()->create([
+            'name' => $request->name,
+            'date' => $request->date,
+            'amount' => $request->amount,
+            'category' => $request->category,
+            'tags' => $request->tags,
+            'notes' => $request->notes,
+        ]);
+
+        return back();
+    }
+}
