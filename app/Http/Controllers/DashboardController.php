867abea459af4451bf82537075df5f2402a59bc0
Matthew Bleathman <24397291+MatthewBleUK@users.noreply.github.com>
2021-09-11 23:16:59 +0100

    Added dashboard number functionality

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,13 +15,25 @@ class DashboardController extends Controller
 
     public function index ()
     {   
-        //dd(auth()->user()->transactions);   //Uses collection  -> helps manipulate lists of data
+        $transactions = Transaction::orderBy('date', 'desc')->get()->where('user_id', auth()->user()->id);
+    
+        $income = $transactions->where('category', 'Income')->sum('amount');
+        $expenses = $transactions->where('category', 'Expense')->sum('amount');
 
-       $transactions = Transaction::orderBy('date', 'desc')->get()->where('user_id', auth()->user()->id);
-       
-       
+        $total = $income - $expenses;
+        $minus = '';
+        
+        if(strpos($total, "-") !== false) {
+            $total = str_ireplace('-', ' ', $total);
+            $minus = '-';
+        } 
+
         return view('dashboard', [
-            'transactions' => $transactions
+            'transactions' => $transactions,
+            'income' => $income,
+            'total' => $total,
+            'minus' => $minus,
+            'expenses' => $expenses
         ]);
     }
 }
